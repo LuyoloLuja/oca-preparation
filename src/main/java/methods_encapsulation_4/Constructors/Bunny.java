aa3bc4f331ee7ceec3a49801f2fbe2ddc67750d0
LuyoloLuja <luyolom19@gmail.com>
2021-11-23 17:50:50 +0200

    started working on constructors

---
+++ src/main/java/methods_encapsulation_4/Constructors/Bunny.java
@@ -0,0 +1,33 @@
+package methods_encapsulation_4.Constructors;
+
+public class Bunny {
+//    common way of writing a constructor
+    private String name;
+    public Bunny(String name) {
+        this.name = name;
+    }
+
+    public Bunny() {
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        new Bunny();
+    }
+}
+class Other {
+    private String color;
+    private int height;
+    private int length;
+
+    public Other(int length, int theHeight) {
+        length = this.length;   // backward - no good
+        height = theHeight;     // fine - because a different name
+        this.color = "white";   // fine but redundant
+    }
+
+    public static void main(String[] args) {
+        Other o = new Other(1, 2);
+        System.out.println(o.length + " " + o.height + " " + o.color);
+    }
+}
---
+++ src/main/java/methods_encapsulation_4/Constructors/DefaultConstructor.java
@@ -0,0 +1,37 @@
+package methods_encapsulation_4.Constructors;
+
+public class DefaultConstructor {
+//    if you don't code a constructor java will create one for you - without a parameter
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor(); // calls the default constructor
+    }
+}
+
+class Rabbit1 {
+    //  has a default constructor - because there is no specified constructor
+}
+class Rabbit2 {
+    // doesn't have a default constructor - coz it has a constructor
+    public Rabbit2() {
+        System.out.println("Rabbit2 Constructor");
+    }
+}
+class Rabbit3 {
+    public Rabbit3(boolean bool){
+        System.out.println("Rabbit3 Constructor");
+    }  // doesn't have a default constructor - coz it has a constructor
+}
+class Rabbit4 {
+    private Rabbit4() {
+        System.out.println("Rabbit4 Constructor");
+    }
+}
+
+class RabbitMultiply {
+    public static void main(String[] args) {
+        Rabbit1 rabbit1 = new Rabbit1();    // calls default constructor - Rabbit1 does not have a specified constructor
+        Rabbit2 rabbit2 = new Rabbit2();    // calls Rabbit2 constructor
+        Rabbit3 rabbit3 = new Rabbit3(false);    // calls Rabbit3 constructor
+//        Rabbit4 rabbit4 = new Rabbit4();    // does not compile - Rabbit4 has a private constructor
+    }
+}
